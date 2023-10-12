20e02173b85e404a502dbc2a50b12e8f6bb7834f
Yorman Valencia <yormanvale@outlook.com>
2023-10-12 16:10:56 -0500

    Solución definitiva de ejercicios practicos

--- PEC1_Ej2_1/script.js
+++ PEC1_Ej2_1/script.js
@@ -4,6 +4,8 @@ const edad = document.getElementById("edad");
 const email = document.getElementById("email");
 const clave = document.getElementById("password");
 const confirmaClave = document.getElementById("password2");
+const camposDeEntrada = document.querySelectorAll('input');
+
 
 // Mostrar mensaje de error
 function mostrarError(input, mensaje) {
@@ -21,92 +23,85 @@ function esquemaExito(input) {
 
 // Validación de edad
 function validarEdad(input) {
-    if(isNaN(input.value) || input.value < 0 || input.value > 1000) {
+    if (isNaN(input.value) || input.value < 0 || input.value > 1000) {
         mostrarError(input, "Edad no válida, debe ser numérica entre 0 y 999");
-    }
-    else {
+    } else {
         esquemaExito(input);
-    }   
+    }
 }
 
 // Validación formato de email
 function validarEmail(input) {
     const expresion = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(expresion.test(input.value.trim())) {
+    if (expresion.test(input.value.trim())) {
         esquemaExito(input);
-    }
-    else{
+    } else {
         mostrarError(input, "El formato de email es inválido");
     }
 }
 
 // Validación formato de contraseña
-function validarPassword(input) {    
+function validarPassword(input) {
     if (input.value.search(/[a-z]/) < 0) {
         mostrarError(input, "La contraseña debe contener letras minúsculas.");
-    }
-    else if (input.value.search(/[A-Z]/) < 0) {
+    } else if (input.value.search(/[A-Z]/) < 0) {
         mostrarError(input, "La contraseña debe contener letras mayúsculas.");
-    }
-    else if (input.value.search(/[0-9]/) < 0) {
+    } else if (input.value.search(/[0-9]/) < 0) {
         mostrarError(input, "La contraseña debe contener dígitos numéricos.");
-    }
-    else if(input.value.search(/[\!\@\#\$\%\^\&\*\(\)\_\+\.\,\;\:\-]/) < 0) {
+    } else if (input.value.search(/[\!\@\#\$\%\^\&\*\(\)\_\+\.\,\;\:\-]/) < 0) {
         mostrarError(input, "La contraseña debe tener caractéres especiales.");
     }
 }
 
-// Validación de campos requeridos
-function campoRequerido(inputArray) {
-    inputArray.forEach(function(input) {
-        if(input.value.trim() === '') {
-            mostrarError(input, `${obtenerCampo(input)} es requerido`);
-        }
-        else {
-            esquemaExito(input);
-        }
-    });   
-}
-
 // Validación de longitud de caracteres en campo de formulario
 function validarLongitudCaracteres(input, min, max) {
-    if(input.value.length < min) {
+    if (input.value.length<min) {
         mostrarError(input, `${obtenerCampo(input)} debe contener mínimo ${min} caracteres`);
     }
-    else if(input.value.length> max) {
-        mostrarError(input, `${obtenerCampo(input)} no debe exceder los ${max} caracteres`);
-    }
-    else {
+    else if(input.value.length>max) {
+        mostrarError(input, `${
+            obtenerCampo(input)
+        } no debe exceder los ${max} caracteres`);
+    } else {
         esquemaExito(input);
     }
 }
 
 // Validar si un valor es numérico
-function esNumerico(input) {         
-    if(isNaN(input.value)) {
-        mostrarError(input, `Este campo ${obtenerCampo(input)} debe ser numérico`);        
-    }
-    else {
-        esquemaExito(input);           
-    }    
-}
-
-// Validación rango numérico
-function rangoNumerico(input, min, max) {    
-    if(input.value < min) {
-        mostrarError(input, `El campo ${obtenerCampo(input)} debe ser mínimo ${min}`);
-    }
-    else if(input.value > max) {
-        mostrarError(input, `El campo ${obtenerCampo(input)} no debe exceder la cantidad de ${max}`);
-    }
-    else {
+function esNumerico(input) {
+    if (isNaN(input.value)) {
+        mostrarError(input, `Este campo ${
+            obtenerCampo(input)
+        } debe ser numérico`);
+    } else {
         esquemaExito(input);
     }
 }
 
+// Validación rango numérico
+function rangoNumerico(input, min, max) {
+    if (input.value<min) {
+        mostrarError(input, `El campo ${obtenerCampo(input)} debe ser mínimo ${min}`);
+    }
+    else if(input.value>max) {
+        mostrarError(input, `El campo ${
+            obtenerCampo(input)
+        } no debe exceder la cantidad de ${max}`);
+    } else {
+        esquemaExito(input);
+    }
+}
+
+// Agregar un oyente de evento "click" a todos los campos de entrada
+camposDeEntrada.forEach(function (input) {
+    input.addEventListener("click", function () {
+        validarCampoRequerido(input);
+    });
+});
+
 // Validar concordancia de las contraseñas
 function validarConcordanciaPassword(input1, input2) {
-    if(input1.value !== input2.value) {
+    if (input1.value !== input2.value) {
         mostrarError(input2, "Las contraseñas no concuerdan");
     }
 }
@@ -116,16 +111,46 @@ function obtenerCampo(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-// Manejo de eventos
-formulario.addEventListener("submit", function(e) {
-    e.preventDefault();
-    campoRequerido([usuario, edad, email, clave, confirmaClave]);
+// Función para validar un solo campo requerido
+function validarCampoRequerido(input) {
+    if (! input.parentElement.classList.contains('error')) {
+        if (input.value.trim() === '') {
+            mostrarError(input, `${
+                obtenerCampo(input)
+            } es requerido`);
+        } else {
+            esquemaExito(input);
+        }
+    }
+}
+
+
+// Añadir oyentes de eventos de entrada para campos requeridos
+usuario.addEventListener("input", function () {
+    validarLongitudCaracteres(usuario, 3, 15);
+    validarCampoRequerido(usuario);
+});
+
+edad.addEventListener("input", function () {
     validarEdad(edad);
-    validarLongitudCaracteres(usuario , 3, 15);
-    validarLongitudCaracteres(clave , 8, Infinity);
-    /* esNumerico(edad);
-    rangoNumerico(edad, 0, 999); */
+    validarCampoRequerido(edad);
+});
+
+
+email.addEventListener("input", function () {
     validarEmail(email);
-    validarPassword(clave);   
+    validarCampoRequerido(email);
+});
+
+clave.addEventListener("input", function () {
+    validarPassword(clave);
+    validarLongitudCaracteres(clave, 8, Infinity);
+    validarCampoRequerido(clave);
+
+});
+
+confirmaClave.addEventListener("input", function () {
     validarConcordanciaPassword(clave, confirmaClave);
+    validarCampoRequerido(confirmaClave);
+
 });
--- PEC1_Ej2_3/script.js
+++ PEC1_Ej2_3/script.js
@@ -4,64 +4,94 @@ const sillas = document.querySelectorAll('.fila .silla:not(.ocupada)');
 const cantSillas = document.getElementById("contador");
 const precioTotal = document.getElementById("total");
 const peliculas = document.getElementById("listaPeliculas");
+const precioTaquillaSpan = document.getElementById("precioTaquilla");
+
+// Agrega un evento para detectar cambios en la película seleccionada
+listaPeliculas.addEventListener("change", function () {
+    calcularDivisaTaquilla();
+    actualizarSeleccion();
+});
+
+// Agrega un evento para detectar cambios en la divisa seleccionada
+listaDivisas.addEventListener("change", function () {
+    calcularDivisaTaquilla();
+    actualizarSeleccion();
+
+});
+
 
 poblarInterfazUI();
 
-let precioTaquilla = +peliculas.value;
 
 // Actualizar contador de sillas seleccionadas
 function actualizarSeleccion() {
     const sillasSeleccionadas = document.querySelectorAll('.fila .silla.seleccionada');
 
-    const indiceSillas = [...sillasSeleccionadas].map(silla => [...sillas].indexOf(silla));
+    const indiceSillas = [... sillasSeleccionadas].map(silla => [... sillas].indexOf(silla));
     localStorage.setItem("sillasSeleccionadas", JSON.stringify(indiceSillas));
 
     cantSillas.innerText = sillasSeleccionadas.length;
-    precioTotal.innerText = sillasSeleccionadas.length * precioTaquilla;
+
+
+    // Llama a la función calcularDivisaTaquilla y actualiza el precio en divisa y la divisa
+    calcularDivisaTaquilla().then(result => { // Calcula el precio total en la divisa seleccionada
+        const precioTotalDivisa = result.precioEnDivisa * sillasSeleccionadas.length;
+        const divisa = result.divisa;
+
+        // Muestra el precio total en el divisa seleccionada en el elemento precioTotal
+        precioTotal.innerText = precioTotalDivisa.toFixed(2) + " " + divisa;
+    }).catch(error => {
+        console.error("Error al calcular la divisa: " + error);
+    });
 }
 
 // Actualizar interfaz de usuario con información almancenada localmente.
 function poblarInterfazUI() {
     const sillasSeleccionadas = JSON.parse(localStorage.getItem("sillasSeleccionadas"));
 
-    if(sillasSeleccionadas !== null && sillasSeleccionadas.length > 0) {
+    if (sillasSeleccionadas !== null && sillasSeleccionadas.length > 0) {
         sillas.forEach((silla, indice) => {
-            if(sillasSeleccionadas.indexOf(indice) > -1) {
+            if (sillasSeleccionadas.indexOf(indice) > -1) {
                 silla.classList.add("seleccionada");
             }
         });
     }
     const indicePeliculaSeleccionada = localStorage.getItem("peliculaSeleccionada");
-    if(indicePeliculaSeleccionada !== null) {
+    if (indicePeliculaSeleccionada !== null) {
         peliculas.selectedIndex = indicePeliculaSeleccionada;
     }
 }
 
 //
-function actualizarDivisa() {    
-    const divisa =listaDivisas.selectedIndex;
+function actualizarDivisa() {
+    const divisa = listaDivisas.selectedIndex;
     const cartelera = peliculas.querySelector("option");
-    
-    const texto= peliculas.getElementsByTagName("option")[divisa].text;
+
+    const texto = peliculas.getElementsByTagName("option")[divisa].text;
     cartelera.innerText = "red";
-    
-    
+
+
 }
 
 // Convertir precio taquilla según divisa
 function calcularDivisaTaquilla() {
-    const divisa = listaDivisas.value;
-    const tarifasCambiarias = peliculas.querySelectorAll("option").values;    
+    const currency1 = listaDivisas.value;
+    const currency2 = "USD";
 
-    fetch(`https://v6.exchangerate-api.com/v6/77698641023fb36ba5bdc2c7/latest/${divisa}`).then(res => res.json()).then(data =>{        
-    const miTasa = data.conversion_rates[divisa];
-    
-    for(let i=0; i < tarifasCambiarias.length; i++) {
-        const nuevaTarifa = (tarifasCambiarias[i].value * miTasa).toFixed(2);
-        tarifasCambiarias[i].innerText = nuevaTarifa;
-    }
-   
-    });  
+    // Obtiene el precio de la película seleccionada (value del select)
+    const precioPelicula = parseInt(listaPeliculas.value);
+
+    // Hace la solicitud a la API de tasas de cambio y retorna la promesa
+    return fetch(`https://v6.exchangerate-api.com/v6/77698641023fb36ba5bdc2c7/latest/${currency1}`).then(res => res.json()).then(data => {
+        const miTasa = data.conversion_rates[currency2];
+
+        // Calcula el precio en la divisa y lo muestra en el span
+        const precioEnDivisa = (precioPelicula * miTasa).toFixed(2);
+        precioTaquillaSpan.textContent = "Precio de la taquilla: " + precioEnDivisa + " " + currency1;
+
+        // Retorna un objeto con el precio en divisa y la divisa
+        return {precioEnDivisa, divisa: currency1};
+    });
 }
 
 // Guardar datos de película y precio
@@ -72,22 +102,26 @@ function guardarDatosPelicula(indicePelicula, precioPelicula) {
 
 // Event Listeners
 
-// Selección de divisa de pago.
-listaDivisas.addEventListener("change", calcularDivisaTaquilla());
-    
+// // Selección de divisa de pago.
+// listaDivisas.addEventListener("change", calcularDivisaTaquilla());
 
+// // Agrega un evento 'change' al select
+// listaPeliculas.addEventListener("change", calcularDivisaTaquilla());
 
 
 // Selección de película
 peliculas.addEventListener("change", e => {
-    precioTaquilla = +e.target.value;   
-    guardarDatosPelicula(e.target.selectedIndex ,e.target.value);
+    precioTaquilla = + e.target.value;
+
+    precioTaquillaSpan.textContent = "Precio de la taquilla: " + precioTaquilla;
+
+    guardarDatosPelicula(e.target.selectedIndex, e.target.value);
     actualizarSeleccion();
-} );
+});
 
 // Selección de sillas
-contenedor.addEventListener("click", e =>{
-    if(e.target.classList.contains('silla') && !e.target.classList.contains('ocupada')) {
+contenedor.addEventListener("click", e => {
+    if (e.target.classList.contains('silla') && !e.target.classList.contains('ocupada')) {
         e.target.classList.toggle('seleccionada');
     }
     actualizarSeleccion();
